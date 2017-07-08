3fe823b19ab3cd70def91ad57f522bbdbf1a281c
Turbo Panumarch <turbo@omise.co>
2017-07-08 22:07:55 +0700

    Refactor tutorial 6

--- server.js
+++ server.js
@@ -1,37 +1,7 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
-const { graphql, buildSchema } = require('graphql')
-const { range } = require('lodash')
 
-const schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
-  }
-
-  type Query {
-    getDie(numSides: Int): RandomDie
-  }
-`)
-
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides
-  }
-
-  rollOnce() {
-    return random(1, this.numSides)
-  }
-
-  roll({ numRolls }) {
-    return range(numRolls).map(_ => this.rollOnce())
-  }
-}
-
-const root = {
-  getDie: ({ numSides }) => new RandomDie(numSides || 6)
-}
+const { schema, root } = require('./tutorial-6')
 
 const app = express()
 app.use('/graphql', graphqlHTTP({
@@ -42,12 +12,3 @@ app.use('/graphql', graphqlHTTP({
 
 app.listen(4000)
 console.log('Running a GraphQL API server at localhost:4000/graphql')
-
-
-/*
- * Helpers
- */
-function random(start, end) {
-  const range = end - start + 1
-  return start + Math.floor(Math.random() * range)
-}
---
+++ tutorial-6.js
@@ -0,0 +1,48 @@
+/*
+ * Tutorial 6: Object Types
+ */
+const { graphql, buildSchema } = require('graphql')
+const { range } = require('lodash')
+
+const schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
+  type Query {
+    getDie(numSides: Int): RandomDie
+  }
+`)
+
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides
+  }
+
+  rollOnce() {
+    return random(1, this.numSides)
+  }
+
+  roll({ numRolls }) {
+    return range(numRolls).map(_ => this.rollOnce())
+  }
+}
+
+const root = {
+  getDie: ({ numSides }) => new RandomDie(numSides || 6)
+}
+
+module.exports = {
+  schema,
+  root,
+}
+
+/*
+ * Helpers
+ */
+function random(start, end) {
+  const range = end - start + 1
+  return start + Math.floor(Math.random() * range)
+}
